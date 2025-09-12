e5e33cd61888056ab229a6baf55050c228338485
ahaghazade <ahaghazade@gmail.com>
2025-09-12 22:37:22 +0330

    update sim800 cdrv

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,22 @@
 
 #include "Sim800_cdrv.h"
 
+
+#define RX2D2 16
+#define TX2D2 17
+
 sSim800 Sim800;
 
 
 void setup() {
 
   Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RX2D2, TX2D2);
 
-  Sim800.Serial = &Serial;
+  Sim800.ComPort = &Serial2;
+  Sim800.CommandSendRetries = 3;
+  Sim800.EnableDeliveryReport = true;
+
   Serial.print("init sim800 : ");
   Serial.println(fSim800_Init(&Sim800));
 
@@ -19,9 +27,16 @@ void setup() {
   Serial.println("Saved phone numbers:");
   serializeJsonPretty(Sim800.SavedPhoneNumbers, Serial);
 
+  // Serial.print("\nsend sms result: ");
+  // Serial.println(fSim800_SendSMS(&Sim800, "09127176496", "1Helloooo"));
+  // Serial.println(fSim800_SendSMS(&Sim800, "09127176496", "2Helloooo"));
+
+  Serial.println("-------");
+
 }
 
 void loop() {
 
-
+  fSim800_Run(&Sim800);
+  delay(5000);
 }
